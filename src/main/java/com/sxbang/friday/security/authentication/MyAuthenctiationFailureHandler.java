fd7f42ee8b7d97c927ee2198ae157de45ef48c53
caidenzhang <772149497@qq.com>
2019-05-31 11:05:34 +0800

    更新登录异常提示信息

--- src/main/java/com/sxbang/friday/security/authentication/MyAuthenctiationFailureHandler.java
+++ src/main/java/com/sxbang/friday/security/authentication/MyAuthenctiationFailureHandler.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.*;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
@@ -13,6 +14,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 @Slf4j
@@ -31,8 +34,21 @@ public class MyAuthenctiationFailureHandler extends SimpleUrlAuthenticationFailu
             response.setHeader("Access-Control-Allow-Methods", "*");
             response.setContentType("application/json;charset=UTF-8");
             response.setStatus(HttpStatus.UNAUTHORIZED.value());
-
-            response.getWriter().write(objectMapper.writeValueAsString(exception));
+            Map<String,Object> map = new HashMap<>();
+            if(exception instanceof LockedException){
+                map.put("message","账户被锁定，登录失败!");
+            }else if(exception instanceof BadCredentialsException){
+                map.put("message","账户名或密码输入错误，登录失败!");
+            }else if(exception instanceof DisabledException){
+                map.put("message","账户被禁用，登录失败!");
+            }else if(exception instanceof AccountExpiredException){
+                map.put("message","账户已过期，登录失败!");
+            }else if(exception instanceof CredentialsExpiredException){
+                map.put("message","密码已过期，登录失败!");
+            }else{
+                map.put("message","登录失败!");
+            }
+            response.getWriter().write(objectMapper.writeValueAsString(map));
         }else{
             super.onAuthenticationFailure(request, response, exception);
         }
